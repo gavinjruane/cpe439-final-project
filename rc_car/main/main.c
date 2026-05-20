833b962a2f2724ca42892d44611d3e3eac040316
Gavin Ruane <gjrswim+github@gmail.com>
2026-05-19 19:21:52 -0700

    [main.c] Added simple LED flashing
    Researching Bluetooth Classic with esp32

--- rc_car/main/main.c
+++ rc_car/main/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
 
-void app_main(void)
-{
+#define GPIO_PIN_2 2
 
+void app_main(void) {
+    gpio_reset_pin(GPIO_PIN_2);
+
+    gpio_set_direction(GPIO_PIN_2, GPIO_MODE_OUTPUT);
+
+    int led_state = 0;
+    while (1) {
+        gpio_set_level(GPIO_PIN_2, led_state);
+
+        printf("LED is %s\n", led_state ? "ON" : "OFF");
+
+        led_state = !led_state;
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
