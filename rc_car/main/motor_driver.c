4798adec1c88064a9b1f2a234f961eb97a6d5046
Gavin Ruane <gjrswim+github@gmail.com>
2026-05-27 15:36:30 -0700

    [main.c] Added example file with PWM control

--- rc_car/main/motor_driver.c
+++ rc_car/main/motor_driver.c
@@ -1,16 +1,16 @@
 
-#include <stdio.h>
-#include "driver/pulse_cnt.h"
-#include "esp_timer.h"
-#include "bdc_motor.h"
-#include "pid_ctrl.h"
+// // #include <stdio.h>
+// // #include "driver/pulse_cnt.h"
+// // // #include "esp_timer.h"
+// // // #include "bdc_motor.h"
+// // // #include "pid_ctrl.h"
 
-/**
- * @brief Sending motor commands through PWM
- * 
- */
-void test_motors(void)
-{
-    gpio_init();
+// /**
+//  * @brief Sending motor commands through PWM
+//  * 
+//  */
+// void test_motors(void)
+// {
+//     gpio_init();
 
-}
+// }
